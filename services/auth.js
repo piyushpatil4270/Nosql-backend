48d000076f9a876df63ff75d0507e59841b5bdb3
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-09-23 18:27:54 +0530

    rebuilt bcrypt from start

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,5 @@
 const nodemailer=require("nodemailer")
-const bcrypt=require("bcrypt")
+const bcrypt=require("bcryptjs")
 const jwt=require("jsonwebtoken")
 const Users=require("../models/users")
 const {v4:uuidv4} =require("uuid")
